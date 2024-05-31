b2b901be65294d1a21ca2859d00004f669908987
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-05-31 11:43:38 +0200

    info displayed

--- script/code.js
+++ script/code.js
@@ -6,7 +6,36 @@
 // }
 
 document.getElementById("btn").addEventListener("click", () => {
-  let firstName = document.getElementById
+
+  let firstName = document.getElementById('firstName').value;
+  document.getElementById('fname').innerText = 'name '+ firstName;
+
+  let lastName = document.getElementById('lastName').value;
+  document.getElementById('lname').innerText = 'surname '+ lastName;
+
+  let age = document.getElementById('age').value;
+  document.getElementById('ageO').textContent = 'age '+ age;
+
+  let dobInput = document.getElementById('dob').value;
+  let dob = new Date(dobInput);
+  document.getElementById('dobO').textContent = ('DOB ') + dob;
+
+  let gender = document.getElementsByName('gender');
+  for(item in gender){
+    if(gender[item].checked)
+      document.getElementById('genderO').textContent =  gender[item].value;
+  }
+  let greet = document.getElementById('greeting').value;
+  document.getElementById('greetingO').textContent = greet;
+
+  // let subject = [];
+  // formElements.subject.forEach((item) => {
+  //     if (item.checked) {
+  //         subject.push(item.value);
+  //     }
+  // });
+  // document.getElementById('subjectO').innerText = subject.join(', ');
+
   let Hours = document.getElementById("hours").value;
   let Rate = document.getElementById("rate").value;
 
